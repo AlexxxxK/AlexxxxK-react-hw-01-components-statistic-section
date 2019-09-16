4bb5510b53bd84562963ab86860ada59a0358e7e
Oleg Kozyr <olegvkozyr@gmail.com>
2019-09-16 08:50:42 +0300

    Updates

---
+++ precache-manifest.b78fe2a5d0f0ad2740bc304df2392484.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f70c42580b6b6304d17d9b0e4d3044",
+    "url": "/react-hw-01-components-statistic-section/index.html"
+  },
+  {
+    "revision": "8711cae664847cda0056",
+    "url": "/react-hw-01-components-statistic-section/static/css/main.6b478e8f.chunk.css"
+  },
+  {
+    "revision": "b1caff781819a2bc1c3b",
+    "url": "/react-hw-01-components-statistic-section/static/js/2.53febdc6.chunk.js"
+  },
+  {
+    "revision": "8711cae664847cda0056",
+    "url": "/react-hw-01-components-statistic-section/static/js/main.27854423.chunk.js"
+  },
+  {
+    "revision": "5614de5f2fa5fff916f8",
+    "url": "/react-hw-01-components-statistic-section/static/js/runtime~main.08301b2c.js"
+  }
+]);
